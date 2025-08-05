c23ed2c62335f8a13a8df1a3ce3c5cc75b7b9536
Yash Jariwala <yashjariwala31003@gmail.com>
2025-08-05 09:07:39 +0530

    remove decryptor.py

--- decryptor.py
+++ decryptor.py
@@ -1,18 +1,1 @@
-from cryptography.fernet import Fernet
-import os
 
-with open("keyfile.key", "rb") as f:
-    key = f.read()
-
-fernet = Fernet(key)
-target_folder = "test_files"
-
-for root, _, files in os.walk(target_folder):
-    for file in files:
-        if file.endswith(".txt"):
-            filepath = os.path.join(root, file)
-            with open(filepath, "rb") as f:
-                data = f.read()
-            decrypted = fernet.decrypt(data)
-            with open(filepath, "wb") as f:
-                f.write(decrypted)
